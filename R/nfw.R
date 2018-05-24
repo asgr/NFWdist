da9de22cbd816b32e23985998e4acb9bbf4e41e1
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-05-24 14:40:26 +0800

    Safer functions.

--- R/nfw.R
+++ R/nfw.R
@@ -9,25 +9,40 @@ dnfw = function(x, con=5, log=FALSE){
     d = con^2*x/((con*x+1)^2*(1/(con+1)+log(con+1)-1))
   }
   d[x>1]=0
+  d[x<=0]=0
   return(d)
 }
 
 pnfw = function(q, con=5, log.p=FALSE){
+  p = .pnfwunorm(q=q, con=con)/.pnfwunorm(q=1, con=con)
+  p[q>1]=1
+  p[q<=0]=0
   if(log.p){
-    p = log(.pnfwunorm(q=q, con=con)/.pnfwunorm(q=1, con=con))
+    return(log(p))
   }else{
-    p = .pnfwunorm(q=q, con=con)/.pnfwunorm(q=1, con=con)
+    return(p)
   }
-  return(p)
 }
 
 qnfw = function(p, con=5, log.p=FALSE){
-  if(log.p){p=exp(p)}
+  if(log.p){
+    p=exp(p)
+  }
+  p[p>1]=1
+  p[p<=0]=0
   p=p*.pnfwunorm(q=1, con=con)
-  return((-(1/lambert_W0(-exp(-p-1)))-1)/con)
+  if(requireNamespace("lamW", quietly = TRUE)){
+    return((-(1/lamW::lambertW0(-exp(-p-1)))-1)/con)
+  }else if(requireNamespace("gsl", quietly = TRUE)){
+    return((-(1/gsl::lambert_W0(-exp(-p-1)))-1)/con)
+  }else{
+    stop('One of lamW (fastest) or gsl (easier to install) package must be installed to use this function!')
+  }
 }
 
 rnfw = function(n, con=5){
-  if(length(n)>1){n=length(n)}
+  if(length(n)>1){
+    n=length(n)
+  }
   return(qnfw(p=runif(n), con=con))
 }
